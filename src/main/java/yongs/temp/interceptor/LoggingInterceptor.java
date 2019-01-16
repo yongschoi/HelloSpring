771eaef132c72153a7c720470c89cc9836e783dc
Yongs <yongschoi@naver.com>
2019-01-16 10:16:34 +0900

    로그

--- src/main/java/yongs/temp/interceptor/LoggingInterceptor.java
+++ src/main/java/yongs/temp/interceptor/LoggingInterceptor.java
@@ -2,24 +2,29 @@ package yongs.temp.interceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
-public class LogInterceptor extends HandlerInterceptorAdapter {
-    private static final Logger logger = LoggerFactory.getLogger(LogInterceptor.class);
+public class LoggingInterceptor extends HandlerInterceptorAdapter {
+    private static final Logger logger = LoggerFactory.getLogger(LoggingInterceptor.class);
     
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
             throws Exception {
         long startTime = System.currentTimeMillis();
-        logger.info("\n-------- LoginInterceptor.preHandle --- ");
+        logger.info("\n-------- LoggingInterceptor.preHandle --- ");
         logger.info("Request URL: " + request.getRequestURL());
         logger.info("Start Time: " + System.currentTimeMillis());
  
         request.setAttribute("startTime", startTime);
+        
+        HttpSession session = request.getSession(false);
+        MDC.put("sessionId", session.getId());
         
         return true;
     }
@@ -27,7 +32,7 @@ public class LogInterceptor extends HandlerInterceptorAdapter {
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
             ModelAndView modelAndView) throws Exception {
-    	logger.info("\n-------- LoginInterceptor.postHandle --- ");
+    	logger.info("\n-------- LoggingInterceptor.postHandle --- ");
     	logger.info("Request URL: " + request.getRequestURL());
  
         // You can add attributes in the modelAndView
@@ -37,7 +42,7 @@ public class LogInterceptor extends HandlerInterceptorAdapter {
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
             throws Exception {
-    	logger.info("\n-------- LoginInterceptor.afterCompletion --- ");
+    	logger.info("\n-------- LoggingInterceptor.afterCompletion --- ");
  
         long startTime = (Long) request.getAttribute("startTime");
         long endTime = System.currentTimeMillis();
